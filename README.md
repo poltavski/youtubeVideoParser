b01ef3444e8c2300150624d4ed3e57031eab6549
Artem Poltavsky <onsub0@gmail.com>
2020-04-28 14:47:58 +0300

    Update parsing from .txt

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # youtubeVideoParser
 Simple terminal python youtube video parser by given query with use of pytube library 
+
+## Installation
+Install requirements by:
+
+```pip install -r requirements.txt```
+
+### Usage
+#### script arguments
+```shell script
+'--query', type=str, default='None',
+help='query for downloading'
+'--output-folder', type=str, default='data',
+help='output folder name'
+'--verbose', type=bool, default=False,
+help='give detailed description of processing'
+'--text-queries', type=str, default='example.txt',
+help='.txt file with queries, check example.txt'
+```
+#### run solution
+From command line or IDE with additional parameters:
+
+``` python youtube_parser.py --text-queries="my_txt_queries.txt" --output_folder="downloads"```
+
--- youtube_parser.py
+++ youtube_parser.py
@@ -12,7 +12,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        '--query', type=str, required=True, default='hello world',
+        '--query', type=str, default='None',
         help='query for downloading'
     )
 
@@ -20,7 +20,45 @@ def parse_args():
         '--output-folder', type=str, default='data',
         help='output folder name'
     )
+
+    parser.add_argument(
+        '--verbose', type=bool, default=False,
+        help='give detailed description of processing'
+    )
+
+    parser.add_argument(
+        '--text-queries', type=str, default='example.txt',
+        help='.txt file with queries, check example.txt'
+    )
+
     return parser.parse_args()
+
+
+def process_query(download_folder, query, verbose):
+    """
+    process single query
+    :param download_folder: str path to download folder
+    :param query: str search query
+    :return: downloads files in download_folder directory
+    """
+    download_path = download_folder + '/' + query
+
+    os.makedirs(download_path, exist_ok=True)
+    url_list = get_url_list(query)
+
+    if len(os.listdir(download_path)) > len(url_list)/2:
+        print('Query: ' + query + ' already downloaded, skip')
+        return
+
+    for i in tqdm(range(len(url_list))):
+        try:
+            YouTube(url_list[i]) \
+                .streams \
+                .get_by_resolution('720p') \
+                .download(download_path)
+        except:
+            if verbose:
+                print('no 720p quality, pass: ', url_list[i])
 
 
 def get_url_list(query):
@@ -44,17 +82,21 @@ def get_url_list(query):
 
 def main():
     args = parse_args()
-
     query = args.query
     download_folder = args.output_folder
-    download_path = download_folder+'/'+query
-    os.makedirs(download_path, exist_ok=True)
-    url_list = get_url_list(query)
-    for i in tqdm(range(len(url_list))):
-        YouTube(url_list[i])\
-            .streams\
-            .get_by_resolution('720p')\
-            .download(download_path)
+    text_queries = args.text_queries
+    verbose = args.verbose
+
+    queries = []
+    if query != 'None':
+        queries.append(query)
+
+    file = open(text_queries, "r")
+    queries.extend(file.read().splitlines())
+    print('Queries to process: ', len(queries))
+    for q in queries:
+        print('Process query: ', q)
+        process_query(download_folder=download_folder, query=q, verbose=verbose)
 
 
 if __name__ == '__main__':
